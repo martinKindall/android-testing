385df14224a8e7fe23ad5ee5c4cba005e096585e
martinKindall <mart256@gmail.com>
2020-12-20 18:52:39 +0100

    Creted Statistics test

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,19 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.junit.Assert.*
+import org.junit.Test
+
+class StatisticsUtilsTest {
+
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
+        val tasks = listOf<Task>(
+                Task("title", "desc", isCompleted = false)
+        )
+
+        val result = getActiveAndCompletedStats(tasks)
+        assertEquals(result.activeTasksPercent, 100f)
+        assertEquals(result.completedTasksPercent, 0f)
+    }
+}
