e8b547f47b8955bf5f3c18509654a85ef08e23c2
martinKindall <mart256@gmail.com>
2020-12-20 23:00:47 +0100

    Updated gradle. Asserting Live data with observe forever

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,9 +1,15 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
 import android.app.Application
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.Event
+import org.hamcrest.Matchers.not
+import org.hamcrest.core.IsNull.nullValue
 import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -11,10 +17,23 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
 
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
     @Test
     fun addNewTask_setsNewTaskEvent() {
         val mockContext = ApplicationProvider.getApplicationContext<Application>()
         val taskViewModel = TasksViewModel(mockContext)
-        taskViewModel.addNewTask()
+
+        val observer = Observer<Event<Unit>> {}
+        try {
+            taskViewModel.newTaskEvent.observeForever(observer)
+            taskViewModel.addNewTask()
+
+            val value = taskViewModel.newTaskEvent.value
+            assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
+        } finally {
+            taskViewModel.newTaskEvent.removeObserver(observer)
+        }
     }
 }
