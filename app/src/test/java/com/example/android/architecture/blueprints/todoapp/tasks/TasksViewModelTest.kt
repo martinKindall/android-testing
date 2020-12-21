b8162714f3a70151d4dfb0b58e949bca4de9ac47
martinKindall <mart256@gmail.com>
2020-12-21 20:56:02 +0100

    Added utils to tests liveData

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.Observer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.architecture.blueprints.todoapp.Event
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.Matchers.not
 import org.hamcrest.core.IsNull.nullValue
 import org.junit.Assert.*
@@ -25,15 +26,8 @@ class TasksViewModelTest {
         val mockContext = ApplicationProvider.getApplicationContext<Application>()
         val taskViewModel = TasksViewModel(mockContext)
 
-        val observer = Observer<Event<Unit>> {}
-        try {
-            taskViewModel.newTaskEvent.observeForever(observer)
-            taskViewModel.addNewTask()
-
-            val value = taskViewModel.newTaskEvent.value
-            assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
-        } finally {
-            taskViewModel.newTaskEvent.removeObserver(observer)
-        }
+        taskViewModel.addNewTask()
+        val newTaskVal = taskViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(newTaskVal?.getContentIfNotHandled(), (not(nullValue())))
     }
 }
