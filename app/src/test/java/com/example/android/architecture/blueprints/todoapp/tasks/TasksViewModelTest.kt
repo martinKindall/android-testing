6290fa5110e68c12618b199b664180dd8d2c9f6c
martinKindall <mart256@gmail.com>
2020-12-21 21:17:41 +0100

    New test to check whether add task button is still present on special filtering

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -2,14 +2,13 @@ package com.example.android.architecture.blueprints.todoapp.tasks
 
 import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Observer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.android.architecture.blueprints.todoapp.Event
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.Matchers.not
 import org.hamcrest.core.IsNull.nullValue
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,13 +20,24 @@ class TasksViewModelTest {
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var taskViewModel: TasksViewModel
+
+    @Before
+    fun setup() {
+        val mockContext = ApplicationProvider.getApplicationContext<Application>()
+        taskViewModel = TasksViewModel(mockContext)
+    }
+
     @Test
     fun addNewTask_setsNewTaskEvent() {
-        val mockContext = ApplicationProvider.getApplicationContext<Application>()
-        val taskViewModel = TasksViewModel(mockContext)
-
         taskViewModel.addNewTask()
         val newTaskVal = taskViewModel.newTaskEvent.getOrAwaitValue()
         assertThat(newTaskVal?.getContentIfNotHandled(), (not(nullValue())))
+    }
+
+    @Test
+    fun setFilterAllTasks_tasksAddViewVisible() {
+        taskViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+        assertTrue(taskViewModel.tasksAddViewVisible.getOrAwaitValue())
     }
 }
